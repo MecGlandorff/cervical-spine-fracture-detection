799b15d79dc43347cda9874ed39afd61260b3180
MecGlandorff <mpjglandorff@gmail.com>
2025-02-15 12:40:58 +0100

    added model arch. to inference

--- inference.py
+++ inference.py
@@ -17,4 +17,66 @@ import timm
 
 
 
+#############
+#Config class
+#############
 
+class Config:
+    data_dir = "data/masks"
+    image_size = 224
+    n_per_slice = 15
+    in_chans = 6
+    out_dim = 1
+
+    # Maybe change in future ?
+    batch_size = 6
+    num_workers = 4
+
+    # I tested other backbones from efficientnet and similar models. They worked, in the future maybe switch to swin transformer and do architecture
+    # overhaul
+    backbone = 'tf_efficientnetv2_s_in21ft1k'
+
+
+
+################################
+# Model definition 
+################################
+class TimmModelWithAttention(nn.Module):
+    def __init__(self, backbone, pretrained=False): # Set pretrained to false because for inference we will load our own model
+        super(TimmModelWithAttention, self).__init__()
+
+        # CNN encoder from timm
+        self.encoder = timm.create_model(
+            backbone, in_chans=Config.in_chans, num_classes=Config.out_dim, pretrained=pretrained
+            )
+        
+        hdim = self.encoder.num_features
+
+        # Remove classifier heda
+        self.encoder.classifier == nn.Identity()
+        
+        # Attention layer 1
+        self.attention1 = nn.Sequential(
+            nn.Linear(hdim, hdim // 2), nn.ReLu(), nn.Linear(hdim // 2, 1), nn.Sigmoid()
+        )
+
+        # Attention layer 2
+        self.attention2 = nn.Sequential(
+            nn.Linear(hdim, hdim // 2), nn.ReLu(), nn.Linear(hdim // 2, 1), nn.Sigmoid()
+        )
+
+        # Attention layer 3
+        self.attention3 = nn.Sequential(
+            nn.Linear(hdim, hdim // 2), nn.ReLu(), nn.Linear(hdim // 2, 1), nn.Sigmoid()
+        )
+
+        # LSTM (bidirect.)
+        self.lstm = nn.LSTM(
+            input_size=hdim, hidden_size=256, num_layers=2, dropout=0.0, bidirectional=True, batch_first=True
+        )
+
+        # Classification head
+        self.head = nn.Sequential(
+            nn.Linear(2*256, 256) # 2*256 because bidirectional
+            , nn.BatchNorm1d(256), nn.Dropout(0.25), nn.LeakyReLu(0.1), nn.Linear(256, Config.out_dim)
+        )
